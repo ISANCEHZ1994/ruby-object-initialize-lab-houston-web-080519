8fd7b9d6055e877d798561eff36189fd2fb98052
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-08 16:17:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  
+  def initialize(name,breed)
+    @name = name
+    @breed = breed
+  end 
+  
+  def name
+   return  @name
+  end
+  
+  def breed=("Mutt")
+    return @breed
+  end
+  
+  
+end
